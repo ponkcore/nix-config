2ecb23d9bb56b2b3b5fe825f1f7e31befe4109c9
Oonishi <oonishi@nixos>
2026-09-20 17:40:31 +0300

    chore(oh-my-pi): bump to v18.2.6

--- pkgs/oh-my-pi/default.nix
+++ pkgs/oh-my-pi/default.nix
@@ -8,11 +8,11 @@
   fetchurl,
 }: let
   pname = "oh-my-pi";
-  version = "18.1.18";
+  version = "18.2.6";
 
   src = fetchurl {
     url = "https://github.com/can1357/oh-my-pi/releases/download/v${version}/omp-linux-x64";
-    hash = "sha256-RUIfml8RK8R8ufd8S015J2Mfj/hZYk+CEofshU6yOfw=";
+    hash = "sha256-DzhZjJHoI9jM4H8VHsOZnVHyE/ssw+B9ifGvju+SR6I=";
   };
 
   inherit (stdenv.cc.bintools) dynamicLinker;
